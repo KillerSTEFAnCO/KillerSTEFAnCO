ec5cbda91bfcf47518bee5053927e14ceb48658f
KillerSTEFAnCO <stefanbakardjiev@gmail.com>
2022-02-02 11:50:18 +0200

    testing

---
+++ sql-query.sql
@@ -0,0 +1,13 @@
+INSERT INTO users (name, password)
+VALUES ('stefan','1234');
+
+
+
+INSERT INTO users (name, password)
+VALUES ('asd','1234');
+INSERT INTO users (name, password)
+VALUES ('dsa','1234');
+INSERT INTO users (name, password)
+VALUES ('asdf','1234');
+
+Select*from users;
